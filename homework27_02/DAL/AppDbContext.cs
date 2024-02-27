a9da25c627a03e5e8b2f177901817c1562436cee
Nadirli Nargiz <nargizzn@code.local>
2024-02-27 17:00:33 +0400

    task

---
+++ homework27_02/DAL/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using homework27_02.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework27_02.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public DbSet<Brand> Brands { get; set;  }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MOON10\\MAINDB;database=BrandDb;trusted_connection=true");
+        }
+      
+
+    }
+}
---
+++ homework27_02/Program.cs
@@ -0,0 +1,39 @@
+﻿
+using homework27_02.DAL;
+using homework27_02.Models;
+
+
+//var brands=context.Brands.ToList();
+
+void AddBrands()
+{
+    AppDbContext context = new AppDbContext();
+    Brand newbrands = new Brand
+    {
+        Name = "Brand1"
+    };
+    context.Brands.Add(newbrands);
+    context.SaveChanges();
+}
+
+
+List<Brand> GetBrands()
+{
+    AppDbContext context = new AppDbContext();
+    return context.Brands.ToList();
+};
+void UpdateNameBrands()
+{
+    AppDbContext context = new AppDbContext();
+    var brand = context.Brands.First();
+    brand.Name = "Brands2";
+    context.SaveChanges();
+}
+void DeleteBrands()
+{
+    AppDbContext context = new AppDbContext();
+    var brand = context.Brands.First();
+    context.Brands.Remove(brand);
+    context.SaveChanges();
+}
+
